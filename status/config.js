122936c12b60bbbb3b88f195902313684a5f4aca
moaradc <143486936+moaradc@users.noreply.github.com>
2024-06-27 21:26:36 +0800

    Add files via upload

---
+++ status/config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '网站监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2595260-a533723261009b1d679553ad',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'bilibili',
+      url: 'https://b23.tv/zbuvkKr'
+    },
+  ],
+};
