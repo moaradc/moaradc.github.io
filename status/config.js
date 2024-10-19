89373bb5c50e8b228d5a85816ba7ba1cbb72ec9f
moaradc <143486936+moaradc@users.noreply.github.com>
2024-10-19 23:10:49 +0800

    Create config.js

---
+++ status/config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '站点监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2595260-84eef541dea84bef121a5c17',
+  ],
+
+  // 日志天数
+  CountDays: 50,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://moaradc.github.io'
+    },
+  ],
+};
