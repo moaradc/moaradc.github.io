508901a3fc3b5242233a4dd3139aa58e1d546f64
moaradc <143486936+moaradc@users.noreply.github.com>
2026-05-03 19:14:39 +0800

    Add files via upload

---
+++ poi/errors/articles.js
@@ -0,0 +1,68 @@
+// 文章预览列表
+const articlesData = [
+{
+  "id": "106",
+  "locked": true,
+  "title": "支付宝集福啦",
+  "date": "2026-02-14 08:14",
+  "last_modified": "2026-02-14 08:14",
+  "category": ["说说"],
+  "type": "note",
+  "content_url": "https://1814804152.v.123pan.cn/1814804152/%E7%9B%B4%E9%93%BE/blog/106.json",
+  "content": "<img src=\"https://1814804152.v.123pan.cn/1814804152/%E7%9B%B4%E9%93%BE/blog/%E5%9B%BE%E7%89%87/Screenshot_20260214_081413.jpg\"><img src=\"https://1814804152.v.123pan.cn/1814804152/%E7%9B%B4%E9%93%BE/blog/%E5%9B%BE%E7%89%87/1771028158284.png\">"
+},
+{
+  "id": "105",
+  "title": "Hello world!",
+  "date": "2025-12-29 17:55",
+  "last_modified": "2025-12-29 17:55",
+  "author": "Admin",
+  "category": ["Demo"],
+  "tags": ["Demo"],
+  "desc": "Hello world!",
+  "content_url": "https://1814804152.v.123pan.cn/1814804152/32791579"
+},
+{
+  "id": "104",
+  "title": "pixiv",
+  "date": "2025-12-26 08:59",
+  "last_modified": "2025-12-26 08:59",
+  "author": "Lamb",
+  "category": ["Demo"],
+  "tags": ["Demo"],
+  "type": "gallery",
+  "desc": " "
+},
+{
+  "id": 103,
+  "locked": true,
+  "title": "代码高亮与排版组件演示",
+  "date": "2025-12-12 13:09",
+  "last_modified": "2025-12-12 17:36",
+  "category": ["Demo"],
+  "tags": ["Demo"],
+  "desc": "代码高亮与排版组件演示"
+},
+{
+  "id": 102,
+  "locked": true,
+  "type": "note",
+  "title": "周五的公园与猫",
+  "date": "2025-11-22 10:30",
+  "last_modified": "2025-11-21 10:32",
+  "category": ["说说","生活"],
+  "tags": ["猫"],
+  "content": "<p>今天天气真不错，在公园里看到了一只非常慵懒的猫。生活就该偶尔停下来发发呆。</p><img src=\"https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?q=80&w=1000&auto=format&fit=crop\"><p>拍完照它就跑了，哈哈。</p><img src=\"https://images.unsplash.com/photo-1573865526739-10659fec78a5?q=80&w=1000&auto=format&fit=crop\"><p>晚上准备去吃顿火锅庆祝一下周五！</p>"
+},
+{
+  "id": 101,
+  "locked": true,
+  "title": "月下独酌：李白诗中的孤独与自由",
+  "date": "2025-11-21 13:09",
+  "last_modified": "2025-11-21 13:09",
+  "category": ["闲谈"],
+  "tags": ["书评"],
+  "desc": "品读李白《月下独酌》，探寻诗仙在孤独中寻得的自由境界。",
+  "image": "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?q=80&w=1000&auto=format&fit=crop"
+}
+];
